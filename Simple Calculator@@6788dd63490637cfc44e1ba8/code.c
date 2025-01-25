db74c6066b6a68d87b0bb793f73a37737fc4c1ea
Bhimanshu29042005 <bhimanshu0197.becse24@chitkara.edu.in>
2025-01-25 19:32:31 +0530

    Solution to Simple Calculator

---
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() {
+    int a, b, sum;
+    char c;
+    if(c == "+"){
+        sum = a + b;
+    }
+    else if(c == "-"){
+        sum = a - b;
+    }
+    else if(c == "*"){
+        sum = a * b;
+    }
+    else if(c == "/"){
+        sum = a / b;
+    }
+    printf("%d", sum);
+
+
+   
+    return 0;
+}
